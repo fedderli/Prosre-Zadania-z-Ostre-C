47026c70eec59311ea563fd9ed11266b8c47e650
karol <kapo@com.pl>
2024-12-03 09:29:57 +0100

    Create class Player

--- Board Game  Simulator/Board Game  Simulator/Program.cs
+++ Board Game  Simulator/Board Game  Simulator/Program.cs
@@ -1,5 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+public class Player
+{
+    public string Name;
+    public int Position;
+    public int Score;
 
+    public void Move()
+    {
+        
+    }
+
+    public void AddScore()
+    {
+        
+    }
+}
 internal class Program
 {
     public static void Main(string[] args)
