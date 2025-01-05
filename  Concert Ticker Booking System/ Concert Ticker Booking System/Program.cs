91f495797fae546118a7c9dc8fda36623259fe85
Filip <filip.flicinski@cyfroweszkoly.pl>
2025-01-05 18:34:53 +0100

    Change fields to properties, add constructors, change data type of availableSeats, commit automatically changes

---  Concert Ticker Booking System/ Concert Ticker Booking System/Program.cs
+++  Concert Ticker Booking System/ Concert Ticker Booking System/Program.cs
@@ -1,16 +1,33 @@
-﻿public class Concert
+﻿using System;
+
+public class Concert
 {
-    public string name;
-    public string date;
-    public string location;
-    public List<int> availableSeats;
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+    public int AvailableSeats { get; set; }
+
+    public Concert(string name, DateTime date, string location, int availableSeats)
+    {
+        Name = name;
+        Date = date;
+        Location = location;
+        AvailableSeats = availableSeats;
+    }
 }
 
 public class Ticket
 {
-    public string concert;
-    public int Price;
-    public int seatNumber;
+    public Concert Concert { get; private set; }
+    public decimal Price { get; private set; }
+    public int SeatNumber { get; private set; }
+
+    public Ticket(Concert concert, decimal price, int seatNumber)
+    {
+        Concert = concert;
+        Price = price;
+        SeatNumber = seatNumber;
+    }
 
     public static void BookTicket()
     {
@@ -34,10 +51,11 @@ public class BookingSystem
     }
 }
 
-internal class Program
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        
+
     }
 }
---  Concert Ticker Booking System/ Concert Ticker Booking System/obj/Debug/net8.0/Concert Ticker Booking System.AssemblyInfo.cs
+++  Concert Ticker Booking System/ Concert Ticker Booking System/obj/Debug/net8.0/Concert Ticker Booking System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Concert Ticker Booking System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca37545c719521c531e19e61adae1df9fa411c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eec499d3620c9d85843afb814161876d9b01a4b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Concert Ticker Booking System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Concert Ticker Booking System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
